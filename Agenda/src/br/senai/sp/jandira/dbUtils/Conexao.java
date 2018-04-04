99420d37535e7f61040182cf5301e84f592fb02f
17259201 <17259201@DSN1271058235.cfp127aluno.local>
2018-04-04 11:30:15 -0300

    Final 04-04

---
+++ Agenda/src/br/senai/sp/jandira/dbUtils/Conexao.java
@@ -0,0 +1,26 @@
+package br.senai.sp.jandira.dbUtils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+
+	private static Connection con; 
+	
+	public static Connection abrirConexao (){
+		con = null;
+		
+		try {
+			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+			String dbURL ="jdbc:ucanaccess://C:/Users/17259201/Documents/GitInf2mb/inf2mb/agenda.accdb";
+			con = DriverManager.getConnection(dbURL);
+			System.out.println("Banco conectado com sucesso...");
+		} catch (Exception erro){
+			System.out.println("Ocorreu um erro na abertura do programa");
+			System.out.println(erro.getMessage());
+		}
+		
+		return con;
+	}
+	
+}
