5d096fd18f70f23d99aeb40e563716b0b8f7bc3f
17259201 <17259201@DSN1271058235.cfp127aluno.local>
2018-04-11 08:11:52 -0300

    06/04

--- Agenda/src/br/senai/sp/jandira/app/Agenda.java
+++ Agenda/src/br/senai/sp/jandira/app/Agenda.java
@@ -1,13 +1,14 @@
 package br.senai.sp.jandira.app;
 
 import br.senai.sp.jandira.dbUtils.Conexao;
+import br.senai.sp.jandira.view.FrmAgenda;
 
 public class Agenda {
 
 	public static void main(String[] args) {
 		
-		Conexao.abrirConexao();
-
+		FrmAgenda agenda = new FrmAgenda();
+		agenda.setVisible(true);
 	}
 
 }
